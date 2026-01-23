7dc252f81018f4a1cabdf52b45fcd4c079986326
Jhonny Moreira <jhonnymoreira@protonmail.com>
2026-01-22 23:41:59 -0300

    fix(utils): `getISODate` type signature

--- src/utils/get-iso-date.ts
+++ src/utils/get-iso-date.ts
@@ -1,9 +1,9 @@
 export function getISODate(
   date: Date | string,
-  options: { timeZone: string | undefined } = { timeZone: 'America/Sao_Paulo' },
+  options?: { timeZone: string },
 ): string {
   const formatter = new Intl.DateTimeFormat('en-CA', {
-    timeZone: options.timeZone,
+    timeZone: options?.timeZone ?? 'America/Sao_Paulo',
     year: 'numeric',
     month: '2-digit',
     day: '2-digit',

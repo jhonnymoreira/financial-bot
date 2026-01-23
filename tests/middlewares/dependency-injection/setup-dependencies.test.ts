1428673e7adbdf68e9d813b3fd259de7d72c95e9
Jhonny Moreira <jhonnymoreira@protonmail.com>
2026-01-23 06:43:47 -0300

    chore(middlewares): remove `constants` dependency from `setup-dependencies`

--- tests/middlewares/dependency-injection/setup-dependencies.test.ts
+++ tests/middlewares/dependency-injection/setup-dependencies.test.ts
@@ -1,43 +1,66 @@
-import * as constants from '@/constants/index.js';
-import { setupDependencies } from '@/middlewares/dependency-injection/setup-dependencies.js';
 import * as services from '@/services/index.js';
-import type { Services } from '@/types/dependency-injection.js';
+import type { AppEnv, Services } from '@/types/index.js';
 
-function uncaptalize<T extends string>(str: T): Uncapitalize<T> {
+class MockAnthropicClient {}
+class MockGoogleSheetsClient {}
+
+vi.mock('@/clients/index.js', () => ({
+  AnthropicClient: MockAnthropicClient,
+  GoogleSheetsClient: MockGoogleSheetsClient,
+}));
+
+vi.mock('@/utils/index.js', async (importOriginal) => {
+  const original = await importOriginal<typeof import('@/utils/index.js')>();
+  return {
+    ...original,
+    getEnvironmentVariables: () => ({
+      ALLOWED_CHAT_IDS: [123],
+      ALLOWED_USER_IDS: [456],
+      GOOGLE_SERVICE_ACCOUNT_CREDENTIALS: 'credentials',
+      SPREADSHEET_ID: 'spreadsheet-id',
+      SPREADSHEET_BACKLOG_SHEET_NAME: 'backlog',
+    }),
+  };
+});
+
+function uncapitalize<T extends string>(str: T): Uncapitalize<T> {
   return str.replace(/^(.)/, (char) => char.toLowerCase()) as Uncapitalize<T>;
 }
-
-const dependencies = setupDependencies(
-  {
-    ANTHROPIC_API_KEY: {
-      async get() {
-        return 'value';
-      },
-    },
-    TELEGRAM_BOT_TOKEN: {
-      async get() {
-        return 'value';
-      },
-    },
-    TELEGRAM_WEBHOOK_SECRET_TOKEN: {
-      async get() {
-        return 'value';
-      },
-    },
-    GOOGLE_SERVICE_ACCOUNT: 'value',
-  },
-  { constants },
-);
 
 describe('middleware dependency injection: setup dependencies', () => {
   describe('services', () => {
     const servicesProperties = Object.keys(services) as (keyof Services)[];
     const mapping = servicesProperties.map((key) => ({
-      property: uncaptalize(key),
+      property: uncapitalize(key),
       instance: key,
     }));
 
-    test.each(mapping)('instantiates $property', ({ property, instance }) => {
+    test.each(mapping)('instantiates $property', async ({
+      property,
+      instance,
+    }) => {
+      const { setupDependencies } = await import(
+        '@/middlewares/dependency-injection/setup-dependencies.js'
+      );
+
+      const dependencies = setupDependencies({
+        TELEGRAM_BOT_TOKEN: {
+          async get() {
+            return 'value';
+          },
+        },
+        TELEGRAM_WEBHOOK_SECRET_TOKEN: {
+          async get() {
+            return 'value';
+          },
+        },
+        ANTHROPIC_API_KEY: {
+          async get() {
+            return 'value';
+          },
+        },
+      } as unknown as AppEnv['Bindings']);
+
       expect(dependencies.services[property]).toBeInstanceOf(
         // biome-ignore lint/performance/noDynamicNamespaceImportAccess: it matters only outside of test scope
         services[instance],
